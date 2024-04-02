f1b0fda013f0403394731f307d6638136f1f0408
martin <tiin.-@hotmail.es>
2024-04-02 15:42:32 -0300

    se agregan mas logos

--- src/components/home/Carrusel.jsx
+++ src/components/home/Carrusel.jsx
@@ -18,6 +18,10 @@ const array = ["https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-
 "https://cdn.worldvectorlogo.com/logos/react-native-1.svg",
  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png",
 "https://www.agiledrop.com/sites/default/files/2020-10/JavascriptLogo.svg",
+"https://www.svgrepo.com/show/376337/node-js.svg",
+"https://logos-world.net/wp-content/uploads/2022/07/Java-Logo.png",
+"https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_original_wordmark_logo_icon_146425.png",
+"https://ajeetchaulagain.com/static/7cb4af597964b0911fe71cb2f8148d64/87351/express-js.png"
 ];
 const Carrusel = () => {
   return (
